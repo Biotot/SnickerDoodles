679e843bb0cdaaa15104aa067f70e7276e152e3f
kencs <kencs@DESKTOP-SUEGPNH>
2018-02-06 17:08:19 -0700

    big upload

--- SnickerDoodles/PainterBase.h
+++ SnickerDoodles/PainterBase.h
@@ -12,7 +12,7 @@ using namespace std;
 #define TYPE_STRING "string"
 #define TYPE_INT "int"
 #define TYPE_FLOAT  "float"
-#define MAX_IMAGES  50
+#define MAX_IMAGES  1000
 
 namespace SnickerDoodles
 {
@@ -24,8 +24,12 @@ namespace SnickerDoodles
 		int m_X; int m_Y; int m_Width; int m_Height;
 		CString m_Text;
 		int m_Color;
+		int m_Priority;
 
 	};
+
+
+
 
 	class PainterBase
 	{
@@ -40,24 +44,25 @@ namespace SnickerDoodles
 		void LoadReadyArray();
 		void LoadDrawingArray();
 
-		HDC HDC_Desktop, Memhdc, Loadedhdc;
+		HDC HDC_Desktop, Memhdc;// , Loadedhdc;
 		HWND TargetWnd;
 		HWND TransWnd;
 		HWND Handle;
 		HBITMAP Membitmap;
 
 		HBRUSH box[10];
+		HBRUSH RedToGreen[12];
+		HBRUSH WardColors[12];
 		HFONT Font;
 		
 		//Text colors
 		COLORREF textColor[4];
-		float cameraPosition[3];
+		float m_CameraPositioncameraPosition[3];
 		int screenWidth, screenHeight;
-		int miniScreenSize, mapLocationX;
+		int miniScreenSize, mapLocationX, mapLocationY;
 		int m_NewImageSize = 0;
 		bool m_ImagesReady = false;
-		bool m_ImagesTransfering = false;
-
+		bool m_Locked;
 
 		DrawnImage m_NewImages[MAX_IMAGES];
 		DrawnImage m_ReadyImages[MAX_IMAGES];
@@ -65,19 +70,18 @@ namespace SnickerDoodles
 
 		void AddImage(DrawnImage tImage);
 	private:
-
-
-		void DrawMiniMapBox(int x, int y, int w, int h, int thickness, int color);
+		
+		void DrawMiniMapMinionBar(int x, int y, int w, int h, int thickness, HBRUSH color);
+		void DrawMiniMapBox(int x, int y, int w, int h, int thickness, HBRUSH color);
 		void DrawMiniMapString(int x, int y, int color, CString text);
 		void DrawDataString(int x, int y, int color, CString text);
 
-		void DrawFilledRect(int x, int y, int w, int h, int color);
-		void DrawBorderBox(int x, int y, int w, int h, int thickness, int color);
+		void DrawFilledRect(int x, int y, int w, int h, HBRUSH color);
+		void DrawBorderBox(int x, int y, int w, int h, int thickness, HBRUSH color);
 
 		void DrawString(int x, int y, int color, CString text);
 		void DrawStringToDC(HDC paintTarget, int x, int y, int color, const char* text);
 
 	};
 
-	
 }
